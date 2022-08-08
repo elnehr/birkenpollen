db414d3453eba9eddaf0d598f1e6779d05d25d85
elnehr <104595482+elnehr@users.noreply.github.com>
2022-08-08 21:24:57 +0200

    output images in single folder

--- cut_images/betula_cropping.py
+++ cut_images/betula_cropping.py
@@ -27,7 +27,7 @@ for index, js in enumerate(json_files):
         jsons_data.loc[len(jsons_data)] = [imagePath, objectnr, label, points] #mistake here: index + i; index is the index of the json file, i is the index of the shape in the json file
 
 
-df = jsons_data.loc[jsons_data['label'] == 'Betula', ]
+df = jsons_data  #.loc[jsons_data['label'] == 'Betula', ]  # if you want to only get the Betula pollen
 df['imagePath'] = df['imagePath'].str.replace('Ã¼', 'ü')
 
 for index, row in df.iterrows():
@@ -38,11 +38,11 @@ for index, row in df.iterrows():
     img = Image.open("betula_jpgs/" + row['imagePath'])
     img = img.crop((x1, y1, x2, y2))
 
-    path = 'cropped_imgs/' + row['imagePath'].split('.')[0].split('M')[-1] + "/"
-    isExist = os.path.exists(path)
-    if not isExist:
-        # Create a new directory because it does not exist
-        os.makedirs(path)
+    path = 'cropped_imgs/' #+ row['imagePath'].split('.')[0] + "/"  # if you want to save the images in a different folder for each file
+    #isExist = os.path.exists(path)
+    #if not isExist:
+    #    # Create a new directory because it does not exist
+    #    os.makedirs(path)
 
     img.save(path + row['imagePath'].split('.')[0] + "_n" + str(row[1]) + ".png")
 
